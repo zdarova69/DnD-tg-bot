b65e53aed729effd0d6452320ab239dd65e0d754
zdarova69 <113101818+zdarova69@users.noreply.github.com>
2024-08-11 16:01:24 +0300

    Delete cl.py

--- cl.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key='sk-579fKdJoDNX5nxitko27C0e3BHjCwKfG', 
-    base_url="https://api.proxyapi.ru/openai/v1",
-    )
