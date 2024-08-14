65f720bb3e80bc8bfd9dd1e3b9ffbb84473795f7
zdarova69 <113101818+zdarova69@users.noreply.github.com>
2024-08-15 00:56:44 +0300

    Update cl.py

--- cl.py
+++ cl.py
@@ -2,9 +2,6 @@ from openai import OpenAI
 # import asyncio
 
 client = OpenAI(
-    # This is the default and can be omitted
-    api_key='sk-579fKdJoDNX5nxitko27C0e3BHjCwKfG', 
-    base_url="https://api.proxyapi.ru/openai/v1",
     )
 
 async def chat_gpt(prompt, task):
